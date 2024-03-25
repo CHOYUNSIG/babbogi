521359c70120062b83ab4af1dc0bffb1966e225e
조윤식 <cho.yunsig56@gmail.com>
2024-03-25 13:24:25 +0900

    실시간 인식으로 교체

--- app/src/main/java/com/example/fridgea/CameraScreen.kt
+++ app/src/main/java/com/example/fridgea/CameraScreen.kt
@@ -5,16 +5,15 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,18 +22,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.window.Dialog
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import kotlinx.coroutines.*
 import java.util.concurrent.Executors
 
 
@@ -63,71 +60,51 @@ fun CameraScreen() {
 
     Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
+    }
 
-        if (isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier
-                    .size(50.dp)
-                    .align(Alignment.Center)
-            )
-        } else {
-            IconButton(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(16.dp),
-                onClick = {
-                    isLoading = true
-                    cameraController.setImageAnalysisAnalyzer(executor) { imageProxy ->
-                        imageProxy.image?.let { image ->
-                            val img = InputImage.fromMediaImage(
-                                image,
-                                imageProxy.imageInfo.rotationDegrees
-                            )
+    if (!isLoading) {
+        runBlocking {
+            launch {
+                isLoading = true
+                cameraController.setImageAnalysisAnalyzer(executor) { imageProxy ->
+                    imageProxy.image?.let { image ->
+                        val img = InputImage.fromMediaImage(
+                            image,
+                            imageProxy.imageInfo.rotationDegrees
+                        )
 
-                            barcodeRecognizer.process(img).addOnCompleteListener { task ->
-                                isLoading = false
-                                text =
-                                    if (!task.isSuccessful) task.exception!!.localizedMessage!!.toString()
-                                    else task.result.joinToString("\n") { barcode ->
-                                        barcode.rawValue?: ""
-                                    }
+                        barcodeRecognizer.process(img).addOnCompleteListener { task ->
+                            text =
+                                if (!task.isSuccessful)
+                                    task.exception!!.localizedMessage!!.toString()
+                                else
+                                    task.result.joinToString("\n") { barcode -> barcode.rawValue?: "" }
 
-                                cameraController.clearImageAnalysisAnalyzer()
-                                imageProxy.close()
-                            }
+                            cameraController.clearImageAnalysisAnalyzer()
+                            imageProxy.close()
+                            isLoading = false
                         }
                     }
                 }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_camera_alt_24),
-                    contentDescription = "",
-                    tint = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.size(54.dp)
-                )
             }
         }
     }
 
-    if (text.isNotEmpty()) {
-        Dialog(onDismissRequest = { text = "" }) {
-            Card(modifier = Modifier.fillMaxWidth(0.8f)) {
-                Text(
-                    text = text,
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .padding(top = 16.dp),
-                    style = MaterialTheme.typography.bodySmall
-                )
-                Button(
-                    onClick = { text = "" },
-                    modifier = Modifier
-                        .align(Alignment.End)
-                        .padding(horizontal = 16.dp, vertical = 16.dp)
-                ) {
-                    Text(text = "Done")
-                }
-            }
+    Row(horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(top = 12.dp)) {
+        ElevatedCard(elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+            modifier = Modifier
+                .width(240.dp)
+                .height(100.dp)
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 16.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
