51bc91eb5bd11ab8b8aaf35e171aa51d0bd209e6
조윤식 <cho.yunsig56@gmail.com>
2024-07-16 16:28:38 +0900

    테마 색상 수정

--- app/src/main/java/com/example/babbogi/ui/theme/Theme.kt
+++ app/src/main/java/com/example/babbogi/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -21,26 +22,22 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Color.Black,
     onPrimary = Color.White,
+    secondary = Color.Black,
     onSecondary = Color.White,
+    tertiary = BabbogiGreen,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
+    background = Color.White,
+    surface = BabbogiGreen,
 )
 
 @Composable
 fun BabbogiTheme(
     darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
