151732bf1d6018de000591a1d7d4ce8c94a3c41b
조윤식 <cho.yunsig56@gmail.com>
2024-08-04 23:19:37 +0900

    Unused Directive 삭제

--- app/src/main/java/com/example/babbogi/ui/view/CustomNavigationBar.kt
+++ app/src/main/java/com/example/babbogi/ui/view/CustomNavigationBar.kt
@@ -20,8 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
