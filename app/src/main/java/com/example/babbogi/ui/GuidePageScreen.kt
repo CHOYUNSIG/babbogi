b4cedb60b70596ba56851b4dee788b72396b2159
조윤식 <cho.yunsig56@gmail.com>
2024-08-07 13:07:00 +0900

    스타일 수정

--- app/src/main/java/com/example/babbogi/ui/GuidePageScreen.kt
+++ app/src/main/java/com/example/babbogi/ui/GuidePageScreen.kt
@@ -10,28 +10,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -81,21 +85,21 @@ fun GuidePageScreen(
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun CustomSlider(
-    modifier: Modifier = Modifier,
     sliderList: List<Int>,
     dotsSize: Dp = 10.dp,
     imageCornerRadius: Dp = 16.dp
 ) {
     val pagerState = rememberPagerState(pageCount = { sliderList.size })
     val scope = rememberCoroutineScope()
+    var width by remember { mutableStateOf<Dp?>(null) }
 
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         HorizontalPager(
             state = pagerState,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.weight(1f, fill = false).run { width?.let { this.width(it) } ?: this },
         ) { page ->
             val pageOffset = (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
             val scaleFactor = 0.75f + (1f - 0.75f) * (1f - pageOffset.absoluteValue)
@@ -104,25 +108,22 @@ private fun CustomSlider(
                     .graphicsLayer {
                         scaleX = scaleFactor
                         scaleY = scaleFactor
-                    }
-                    .alpha(scaleFactor.coerceIn(0f, 1f))
-                    .wrapContentSize(),
+                    },
                 shape = RoundedCornerShape(imageCornerRadius),
             ) {
+                val density = LocalDensity.current
                 Image(
                     painter = painterResource(id = sliderList[page]),
                     contentDescription = "Image",
-                    contentScale = ContentScale.FillWidth,
-                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .weight(1f, fill = false)
+                        .onGloballyPositioned { width = density.run { it.size.width.toDp() } },
                 )
             }
         }
         Row(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 16.dp) // 하단 여백 설정
-                .height(50.dp)
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
             horizontalArrangement = Arrangement.Center
         ) {
             repeat(sliderList.size) {
@@ -139,15 +140,18 @@ private fun CustomSlider(
     }
 }
 
-
-
 @Composable
 private fun GuidePage(
     list: List<Int>,
     onComplete: () -> Unit,
 ) {
     ColumnScreen(prohibitScroll = true) {
-        CustomSlider(modifier = Modifier.fillMaxWidth(), sliderList = list)
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            CustomSlider(sliderList = list)
+        }
     }
 
     Box(
